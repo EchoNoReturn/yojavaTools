64bae9fd2b50036576b7bbb83aaa61052f37f840
李建国 <95645011+EchoNoReturn@users.noreply.github.com>
2024-04-21 09:52:35 +0800

    🎉 init: 初始化

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EchoNoReturn/yojavaTools
+
+go 1.22.0
---
+++ utils/ostools.go
@@ -0,0 +1,29 @@
+package utilsOsTools
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+)
+
+/*
+获取系统的类型
+*/
+func getOsType() string {
+	return runtime.GOROOT()
+}
+
+func useOsAdept(winfn func(), linuxfn func(), macfn func()) {
+	err := errors.New("not support")
+	switch os.Getegid() {
+	case 0:
+		winfn()
+	case 1:
+		linuxfn()
+	case 2:
+		macfn()
+	default:
+		fmt.Println(err)
+	}
+}
